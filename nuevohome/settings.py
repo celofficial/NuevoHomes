8d238304d437823013b05e8e4034fbf1c248cc5f
mr-shakib <contactshakibhere@gmail.com>
2024-05-26 21:37:23 +0600

    Updated For Hosting on Vercel

--- nuevohome/settings.py
+++ nuevohome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a$dt4*avxv+wwb=o#cb7!o5f4v3=h69nx(3j!ci0hnb8zxu3tv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -76,9 +77,13 @@ WSGI_APPLICATION = "nuevohome.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gzvNosTfrIWZJHQWWfAAubQYONssXgAx',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '49220',
     }
 }
 
@@ -124,3 +129,6 @@ STATIC_URL = "/static/"  # Add a leading slash
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
